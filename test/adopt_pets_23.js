155ce74a85265a10cc6fc14df4f7fbe11a061f73
RiccardoCecco <riccardocecco1997@gmail.com>
2022-06-10 18:27:17 +0200

    finish

---
+++ test/adopt_pets_23.js
@@ -0,0 +1,55 @@
+const Adoption = artifacts.require("Adoption");
+const { performance } = require('perf_hooks');
+
+// Utils
+const ether = (n) => {
+  return new web3.utils.BN(
+    web3.utils.toWei(n.toString(), 'ether')
+  )
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+module.exports = async function(callback) {
+  try {
+    // Fetch accounts from wallet - these are unlocked
+    const accounts = await web3.eth.getAccounts()
+
+    console.log(`I am ${accounts[22]}`)
+
+    //console.log('Exchange fetched', exchange.address)
+
+    var initialTime = performance.now()
+    var endTime
+    var startTime
+
+    while (true){
+      startTime = performance.now()
+      const r_n = getRandomInt(50)
+      const adoption = await Adoption.deployed()
+
+      const free = await adoption.isExisting(r_n)
+
+      if (free) {
+        //const adopters = await adoption.getAdopters();
+        await adoption.adopt(r_n, {from: accounts[22]})
+        console.log(`Pet ${r_n} has been adopted`)
+        endTime = performance.now()
+        console.log(`TimeTransaction: ${endTime - startTime} ms`)
+        break
+      }else {
+        console.log(`Pet ${r_n} already adopted`)
+        endTime = performance.now()
+        console.log(`TimeTransaction: ${endTime - startTime} ms`)
+      }
+    }
+    console.log(`TotalTime ${endTime - initialTime} ms`)
+  }
+  catch(error) {
+    console.log(error)
+  }
+
+  callback()
+}
